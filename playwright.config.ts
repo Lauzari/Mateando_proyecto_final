4e36b2942ff5d8c08124ba24bb343f3f35f5f93f
Lauzari <mlauramoyano1985@gmail.com>
2026-05-12 13:20:41 -0300

    Add Playwright package and config to Mateando_proyecto_final repo

---
+++ playwright.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from '@playwright/test'
+
+export default defineConfig({
+  testDir: './tests/e2e',
+
+  use: {
+    baseURL: 'https://mateando-tup.onrender.com',
+
+    headless: false,
+
+    screenshot: 'only-on-failure',
+
+    video: 'retain-on-failure',
+
+    trace: 'on-first-retry',
+  },
+
+  reporter: [['html', { outputFolder: 'playwright-report' }]],
+})
